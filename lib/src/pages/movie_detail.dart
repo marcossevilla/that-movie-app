6edddd65d93fc4672f9dd1872911f6ba9dd17605
Marcos Sevilla <marcossevilla14@gmail.com>
2019-06-18 20:36:31 -0600

    finished movie detail page design

--- lib/src/pages/movie_detail.dart
+++ lib/src/pages/movie_detail.dart
@@ -12,6 +12,15 @@ class MovieDetail extends StatelessWidget {
       body: CustomScrollView(
         slivers: <Widget>[
           _createAppBar(movie),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(
+                height: 10.0,
+              ),
+              _postTitle(context, movie),
+              _overview(movie),
+            ]),
+          ),
         ],
       ),
     );
@@ -35,6 +44,63 @@ class MovieDetail extends StatelessWidget {
             placeholder: AssetImage('assets/img/no-image.jpg'),
             fadeInDuration: Duration(microseconds: 100),
             fit: BoxFit.cover),
+      ),
+    );
+  }
+
+  Widget _postTitle(BuildContext context, Movie movie) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15.0),
+            child: Image(
+              image: NetworkImage(movie.getPosterImage()),
+              height: 150.0,
+            ),
+          ),
+          SizedBox(
+            width: 30.0,
+          ),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  movie.title,
+                  style: Theme.of(context).textTheme.title,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  movie.originalTitle,
+                  style: Theme.of(context).textTheme.subhead,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.star),
+                    SizedBox(width: 5.0),
+                    Text(
+                      movie.voteAverage.toString(),
+                      style: Theme.of(context).textTheme.subtitle,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _overview(Movie movie) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 20.0),
+      child: Text(
+        movie.overview,
+        textAlign: TextAlign.left,
       ),
     );
   }
