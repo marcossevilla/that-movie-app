63928cc5dda17f26ea9afa70002917f1f50a1eb6
Marcos Sevilla <marcossevilla14@gmail.com>
2019-06-22 09:23:40 -0600

    added search delegate

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -21,7 +21,9 @@ class HomePage extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: () {},
+            onPressed: () {
+              showSearch(context: context, delegate: );
+            },
           ),
         ],
       ),
---
+++ lib/src/search/search_delegate.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class DataSeach extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // implement buildActions
+    return null;
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // implement buildLeading
+    return null;
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // implement buildResults
+    return null;
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // implement buildSuggestions
+    return null;
+  }
+}
